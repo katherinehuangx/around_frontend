cadd7fcc28483a208dd578799a47b0e9eb4b5eec
katherinehuangx <katherinehuang.su@gmail.com>
2021-11-25 11:50:43 -0500

    added around frontend code

---
+++ around-web/src/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'https://around-330123.uc.r.appspot.com'; // change to deploy url
+//export const BASE_URL = 'http://xxx.xxx.xxx.xxx:8080'; // change to GCE external IP. Choose 1 of the 2 BASE_URL.
+
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
